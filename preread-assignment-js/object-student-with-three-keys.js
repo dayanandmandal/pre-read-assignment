a2d48b132041220a8432785a632640b71585a846
dayanandmandal <120941312+dayanandmandal@users.noreply.github.com>
2023-01-08 22:07:52 +0530

    Update object-student-with-three-keys.js

--- preread-assignment-js/object-student-with-three-keys.js
+++ preread-assignment-js/object-student-with-three-keys.js
@@ -1,26 +1,20 @@
-class Student
-{
-    constructor(id, name, city)
-    {
-        this.id = id;
-        this.name = name;
-        this.city = city;
-    }
-    nameExist()
-    {
-        if( this.name === "")
+let Student = {
+    id : 1,
+    name : "Dayanand",
+    city : "Delhi",
+
+    nameExists : function() {
+        if(this.name === "")
             return false;
         else
             return true;
-    }
+    },
 
-    updateId( id )
-    {
+    updateId : function(id){
         this.id = id;
     }
-}
+};
 
-let student = new Student(1, "dayanand", "Noida");
-// console.log(student.nameExist());
-student.updateId(2);
-console.log(student);
+
+Student.updateId(2);
+console.log(Student);
